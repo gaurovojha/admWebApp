508b9889eaa143a8928dcb4eba907dc061eeff3e
Ojha <ojhag1@global.anz.com>
2017-06-23 19:48:54 +0530

    Code add to integrate mongodb

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,4 @@
+var School = require('../server/model/school');
 module.exports = function(app) {
 
 	// server routes ===========================================================
@@ -6,8 +7,37 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
+	app.get('/home', function(req, res) {
 		res.sendfile('./public/index.html');
+	});
+
+	app.get('/schools', function(req, res) {
+		School.find({}, function(err, schools){
+			console.log(schools);
+			res.send(JSON.stringify(schools));
+		});
+	});
+
+	app.get('/school/register', function(req, res) {
+		var newSchool = School({
+			"_id" : 1,
+        "name" : "Test School",
+        "description" : "Test School description",
+        "location" : "Test School location",
+        "likes" : "10",
+        "comments" : [
+                "Test School comment-1",
+                "Test School comment-1",
+                "Test School comment-1"
+        ]
+		});
+
+		newSchool.save(function(err){
+			if(err)
+				console.log("school could not be created!!!", err);
+			else
+			console.log("school created!!!");
+		});
 	});
 
 };
--- server.js
+++ server.js
@@ -4,15 +4,14 @@ var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
 // configuration ===========================================
 	
 // config files
 var db = require('./config/db');
 
 var port = process.env.PORT || 8081; // set our port
-// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
-
+// connect to our mongoDB database (commented out after you enter in your own credentials)
+mongoose.connect(db.url);
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
